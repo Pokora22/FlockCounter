59d5fe6de2a7782309b741df0c26e91aa1b20f6d
Pokora22 <pokora222@gmail.com>
2019-03-26 15:15:23 +0000

    Merge branch 'indev' of C:\FlockCounter with conflicts.

--- src/sample/SetUtils.java
+++ src/sample/SetUtils.java
@@ -1,21 +1,22 @@
 package sample;
 
-import javafx.scene.chart.XYChart;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class SetUtils {
-    private int[] pixels;
-    private ImageProcessor imgProc;
+public class SetUtils implements Runnable{
+    private int[] arr;
+    private ArrayList<Integer> roots;
+    private HashMap<Integer, Integer> setSizes;
 
-    public SetUtils(int arrSize, ImageProcessor imgProc){
-        pixels = new int[arrSize];
-        this.imgProc = imgProc;
+    public SetUtils(int[] arr){
+        this.arr = arr;
+        roots = new ArrayList<>();
+        setSizes = new HashMap<>();
     }
 
-    public int[] getSortedSets(){
-        int[] sets = pixels.clone();
+    public int[] getSortedSets(int[] arr){
+        int[] sets = arr.clone();
         String unsorted = "Unsorted array: ";
         String sorted = "Sorted array: ";
 
@@ -29,31 +30,57 @@ public class SetUtils {
         return sets;
     }
 
-    public int getRoot(int position){
-        if(pixels[position]<0) {
-            System.out.println(imgProc.getPixelXY(position)[0] + ", " + imgProc.getPixelXY(position)[1]);
+    private void fillSets(){
+        for(int i : arr){
+            if(i >= 0) {
+                int root = findRoot(i);
+                if (!roots.contains(root)){
+                    roots.add(root);
+                    setSizes.put(root, 1);
+                }
+                else setSizes.replace(root, setSizes.get(root)+1);
+            }
         }
-        return pixels[position] == position? position : getRoot(pixels[position]);
+    }
+
+    public int findRoot(int position){
+        return arr[position] == position? position : findRoot(arr[position]);
     }
 
     public ArrayList<Integer> getRoots(){
-        ArrayList<Integer> roots = new ArrayList<>();
-
-        for(int p : pixels){
-            if(p >= 0) {
-                int root = getRoot(p);
-                if (!roots.contains(root)) roots.add(root);
-            }
-        }
-
         return roots;
     }
 
-    public int[] getSets() {
-        return pixels;
+    public ArrayList<Integer> getSizeFilteredRoots(int sizeLimit){
+        ArrayList<Integer> filteredRoots = new ArrayList<>(roots);
+//        int index = 0;
+//        while(index < filteredRoots.size()){
+//            int elemCount = 0;
+//            for(int e : arr){
+//                if(e >= 0 && findRoot(e, arr) == filteredRoots.get(index)) elemCount++;
+//            }
+//            if(elemCount <= sizeLimit){
+//                filteredRoots.remove((Integer)filteredRoots.get(index));
+//                continue;
+//            }
+//            index++;
+//        }
+
+        for(Integer root : roots) if(setSizes.get(root) > sizeLimit) filteredRoots.add(root);
+
+        return filteredRoots;
     }
 
     public void join(int childPosition, int parentPosition) {
-        pixels[childPosition] = parentPosition;
+        arr[findRoot(childPosition)] = findRoot(parentPosition);
+    }
+
+    public int[] getArr(){
+        return arr;
+    }
+
+    @Override
+    public void run() {
+        fillSets();
     }
 }
